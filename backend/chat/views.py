ec37b812bd01f117bd8bc2de501c63b5303aecc4
stanlley-locke <stanlleylocke@gmail.com>
2025-12-10 00:14:03 +0300

    added archive feature

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,7 +1,7 @@
 # backend/chat/views.py
 from rest_framework import status, generics
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, parser_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework_simplejwt.tokens import RefreshToken
